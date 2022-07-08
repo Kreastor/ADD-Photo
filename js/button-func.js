9ada162be86549e8605ba6765312967ea97beacf
Alexandr Kravchuk <sascha977@yandex.ru>
2022-07-08 22:29:38 +0300

    fix: fix some bugs (Friday, July 08, 2022 22:29:30 PM)

--- js/button-func.js
+++ js/button-func.js
@@ -1,8 +1,8 @@
 "use strict"
+// start-screen
 
 const getConsultation = document.querySelector(".start-screen__get-consultation-btn");
 const feedbackForm = document.querySelector(".feedback-form");
-
 
 getConsultation.addEventListener("click", () => {
   let scroll = document.querySelector(".start-screen");
@@ -14,31 +14,76 @@ getConsultation.addEventListener("click", () => {
 });
 
 const sendForm = document.querySelector(".feedback-form__input-btn");
-const applicationFeedback = document.querySelector(".application-feedback")
+const applicationFeedback = document.querySelector(".application-feedback");
 
 sendForm.addEventListener("click", () => {
   feedbackForm.classList.remove("show");
   applicationFeedback.classList.add("show");
 })
 
-const closeBtn = document.getElementsByClassName("feedback-form__close-btn");
+const closeFeedbackBtn = document.querySelector(".feedback-form__close-btn")
 
-for(let i = 0; i < closeBtn.length; i++) {
-  closeBtn[i].addEventListener("click", () => {
-      feedbackForm.classList.remove("show");
-      if (applicationFeedback.classList.contains("show")) {
-        applicationFeedback.classList.remove("show");
-      }
-  });
-};
+closeFeedbackBtn.addEventListener("click", (event) => {
+  feedbackForm.classList.remove("show");
+  applicationFeedback.classList.remove("show");
+})
 
-// -----------------------------------------------------studio-decoration---------------------------------------------- 
+
+const closeApplicationBTN = document.querySelector(".application-feedback__close-btn");
+
+closeApplicationBTN.addEventListener("click", () => {
+    feedbackForm.classList.remove("show");
+    if (applicationFeedback.classList.contains("show")) {
+      applicationFeedback.classList.remove("show");
+    }
+});
+
+
+// our-location-form
+
+const chooseLocationBTN = document.querySelector(".our-location__choose-location-btn");
+const form = document.querySelector(".form-section");
+
+chooseLocationBTN.addEventListener("click", (event) => {
+  let scroll = document.querySelector(".our-location");
+  scroll.scrollIntoView({block: "center", behavior: "smooth"});
+  form.style.display = "block";
+  if (applicationFeedback2.classList.contains("show")) {
+    applicationFeedback2.classList.remove("show");
+  }
+})
+
+const closeLocationForm = document.querySelector(".form-section__close-btn");
+
+closeLocationForm.addEventListener("click", (event) => {
+  form.style.display = "";
+})
+
+const sendLocationForm = document.querySelector(".form-section__input-btn");
+const applicationFeedback2 = document.querySelector(".application-feedback2");
+
+sendLocationForm.addEventListener("click", (event) => {
+  form.style.display = "";
+  applicationFeedback2.classList.add("show");
+})
+
+const closeApplicationBTN2 = document.querySelector(".application-feedback2__close-btn");
+
+closeApplicationBTN2.addEventListener("click", () => {
+  form.style.display = "";
+  if (applicationFeedback2.classList.contains("show")) {
+    applicationFeedback2.classList.remove("show");
+  }
+});
+
+// studio-decoration
 
 const videoСontainer = document.querySelector(".studio-decoration__video-container");
 videoСontainer.addEventListener("click", (event) => {
-  let video = document.querySelector(".studio-decoration__video");
+  alert("Здесь можно разместить видео по усмотрению заказчкика");
+/*let video = document.querySelector(".studio-decoration__video");
   video.style.visibility ="visible";
-  videoСontainer.style.visibility ="hidden";
+  videoСontainer.style.visibility ="hidden"; */
 })
 
 
